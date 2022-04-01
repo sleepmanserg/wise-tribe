1030b442d008b95c280c55a6308a8d082719e057
sleepmanserg <sleepmanserg@gmail.com>
2022-04-01 17:21:18 +0300

    Header added

--- src/js/theme.js
+++ src/js/theme.js
@@ -2,5 +2,32 @@
 // Custom Scripts
 // --------------------------------------------------
 
-let hello = 1;
-console.log(hello);
+/** toggle btn */
+
+document.addEventListener('click', e => {
+	const isTargetButton = e.target.matches('[data-target]');
+	if (!isTargetButton && e.target.closest('[data-active]') != null) return;
+	let currentTarget;
+	if (isTargetButton) {
+		currentTarget = e.target.closest('[data-active]');
+		currentTarget.classList.toggle('active');
+	}
+	document.querySelectorAll('[data-active].active').forEach(item => {
+		if (item == currentTarget) return;
+		item.classList.remove('active');
+	});
+});
+
+/** Menu toggle */
+
+const menuToggle = document.querySelector('.header-menu__toggle');
+
+if (menuToggle) {
+	const menuInner = document.querySelector('.header-inner');
+	menuToggle.addEventListener('click', () => {
+		document.body.classList.toggle('overflow-hidden');
+		menuToggle.classList.toggle('active');
+		menuInner.classList.toggle('active');
+	});
+}
+
